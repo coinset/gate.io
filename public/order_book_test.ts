16eff4d2fcf388fa3f96bc994b2afbe9a78ed170
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-01-20 22:55:53 +0900

    test(public): fix test case

--- public/order_book_test.ts
+++ public/order_book_test.ts
@@ -2,9 +2,7 @@ import { anyArray, anyNumber, expect, test } from "../dev_deps.ts";
 import { fetchOrderBook } from "./order_book.ts";
 
 test("fetchOrderBook", async () => {
-  await expect(fetchOrderBook("BTC_USD", {
-    with_id: true,
-  })).resolves.toEqual({
+  await expect(fetchOrderBook("BTC_USD")).resolves.toEqual({
     current: anyNumber(),
     update: anyNumber(),
     asks: anyArray([anyNumber(), anyNumber()]),
