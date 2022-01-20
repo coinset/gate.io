f7b59e7dcec29b8b5eaa3f9172c257dfa03085e3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-01-20 14:57:56 +0900

    chore(public): remove duplicated types

--- public/ticker_test.ts
+++ public/ticker_test.ts
@@ -1,16 +1,25 @@
 import { fetchTicker } from "./ticker.ts";
-import { anyNumber, anyString, expect, test } from "../dev_deps.ts";
+import { anyNumber, anyOf, anyString, expect, test } from "../dev_deps.ts";
+
+const baseEquality = {
+  currency_pair: anyString(),
+  last: anyNumber(),
+  lowest_ask: anyNumber(),
+  highest_bid: anyNumber(),
+  change_percentage: anyNumber(),
+  base_volume: anyNumber(),
+  quote_volume: anyNumber(),
+  high_24h: anyNumber(),
+  low_24h: anyNumber(),
+};
+export const equality = anyOf([baseEquality, {
+  ...baseEquality,
+  etf_net_value: anyNumber(),
+  etf_pre_net_value: anyNumber(),
+  etf_pre_timestamp: anyNumber(),
+  etf_leverage: anyNumber(),
+}]);
 
 test("fetchTicker", async () => {
-  await expect(fetchTicker("BTC_USD")).resolves.toEqual({
-    currency_pair: anyString(),
-    last: anyNumber(),
-    lowest_ask: anyNumber(),
-    highest_bid: anyNumber(),
-    change_percentage: anyNumber(),
-    base_volume: anyNumber(),
-    quote_volume: anyNumber(),
-    high_24h: anyNumber(),
-    low_24h: anyNumber(),
-  });
+  await expect(fetchTicker("BTC_USD")).resolves.toEqual(equality);
 });
--- public/tickers_test.ts
+++ public/tickers_test.ts
@@ -1,31 +1,7 @@
 import { fetchTickers } from "./tickers.ts";
-import {
-  anyArray,
-  anyNumber,
-  anyOf,
-  anyString,
-  expect,
-  test,
-} from "../dev_deps.ts";
-
-const baseEquality = {
-  currency_pair: anyString(),
-  last: anyNumber(),
-  lowest_ask: anyNumber(),
-  highest_bid: anyNumber(),
-  change_percentage: anyNumber(),
-  base_volume: anyNumber(),
-  quote_volume: anyNumber(),
-  high_24h: anyNumber(),
-  low_24h: anyNumber(),
-};
+import { anyArray, expect, test } from "../dev_deps.ts";
+import { equality } from "./ticker_test.ts";
 
 test("fetchTickers", async () => {
-  await expect(fetchTickers()).resolves.toEqual(anyArray(anyOf([baseEquality, {
-    ...baseEquality,
-    etf_net_value: anyNumber(),
-    etf_pre_net_value: anyNumber(),
-    etf_pre_timestamp: anyNumber(),
-    etf_leverage: anyNumber(),
-  }])));
+  await expect(fetchTickers()).resolves.toEqual(anyArray(equality));
 });
